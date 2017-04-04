1b15ee823cb2083857a4ec91df539e94b195110b
Unknown <davidnunovelez@gmail.com>
2017-04-04 21:52:32 +0200

    Ultimo 04/04/2017

--- Proyecto/src/Modelo/ControladorBaseDatos.java
+++ Proyecto/src/Modelo/ControladorBaseDatos.java
@@ -10,17 +10,27 @@ import java.util.logging.Logger;
 
 public class ControladorBaseDatos {
     
+    private Connection conexion;
+    
     public void crearConexion() throws ClassNotFoundException{
    
        Class.forName("oracle.jdbc.driver.OracleDriver");
         try {   
-            Connection conexion= DriverManager.getConnection("jdbc:oracle:thin:@10.10.10.9:1521:db12102","Aplicacion", "a12345Abcde");
+            conexion= DriverManager.getConnection("jdbc:oracle:thin:@10.10.10.9:1521:db12102","Aplicacion", "a12345Abcde");
         } catch (SQLException ex) {
             Logger.getLogger(General.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-   
+  
         }
+
+    public Connection getConexion() {
+        return conexion;
+    }
+
+    public void setConexion(Connection conexion) {
+        this.conexion = conexion;
+    }
     
 }
 
